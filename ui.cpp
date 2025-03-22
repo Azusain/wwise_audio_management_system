9ed70690274aef136ad40f95a36439878e30ab86
Azusain <azusaings@gmail.com>
2025-03-22 18:24:54 +0800

    chore: typo

--- ui.cpp
+++ ui.cpp
@@ -5,7 +5,7 @@
 #include <string>
 
 // TODO: the max buffer size should be considered carefully.
-// format of the buffer:
+// Format of the buffer:
 // +-------------------------------+-------------+-----------------+
 // | directory_path or file_path_1 | file_path_2 | file_path_n ... |
 // +-------------------------------+-------------+-----------------+
@@ -20,8 +20,7 @@ bool OpenFileDialogMultiSelect(std::vector<std::string>& selected_file_paths, in
   if (GetOpenFileNameW(&ofn)) {
     size_t first_file_or_dir_size_wchar = wcslen(szFile.data());
     wchar_t* p = szFile.data() + first_file_or_dir_size_wchar + 1;
-
-    // only contains a single file.
+    // Only contains a single file.
     if (*p == '\0') {
       // Convert wchar_t path to UTF-8 string
       int len = WideCharToMultiByte(CP_UTF8, 0, szFile.data(), first_file_or_dir_size_wchar, nullptr, 0, nullptr, nullptr);
@@ -29,20 +28,20 @@ bool OpenFileDialogMultiSelect(std::vector<std::string>& selected_file_paths, in
       WideCharToMultiByte(CP_UTF8, 0, szFile.data(), first_file_or_dir_size_wchar, &file_path[0], len, nullptr, nullptr);
       selected_file_paths.emplace_back(file_path);
     }
-    // multiple files, buffer starts with the directory's path.
+    // Multiple files, buffer starts with the directory's path.
     else {
       // Convert wchar_t directory path to UTF-8 string
       int len = WideCharToMultiByte(CP_UTF8, 0, szFile.data(), first_file_or_dir_size_wchar, nullptr, 0, nullptr, nullptr);
       std::string dir(len, 0);
       WideCharToMultiByte(CP_UTF8, 0, szFile.data(), first_file_or_dir_size_wchar, &dir[0], len, nullptr, nullptr);
-
       while (*p) {
         size_t len_path = wcslen(p);
         int path_len = WideCharToMultiByte(CP_UTF8, 0, p, len_path, nullptr, 0, nullptr, nullptr);
         std::string file_path(path_len, 0);
         WideCharToMultiByte(CP_UTF8, 0, p, len_path, &file_path[0], path_len, nullptr, nullptr);
         selected_file_paths.emplace_back(dir + "\\" + file_path);
-        p += len_path + 1; // Move to next file path
+        // Move to next file path
+        p += len_path + 1; 
       }
     }
   }
